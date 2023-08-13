61e096f18bd6de69dfc0cdee6422e3b1c5c7812a
Dmitrii Kulakov <koolda@yandex.ru>
2023-08-13 16:00:17 +0300

    before delete

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -14,19 +14,6 @@ public class SpringBootSecurityDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
 
-		RestTemplate restTemplate = new RestTemplate();
-
-		Map<String, String> jsonToSend = new HashMap<>();
-		jsonToSend.put("name", "Test name");
-		jsonToSend.put("job", "Test job");
-
-		HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSend);
-
-		String url = "https://reqres.in/api/users/";
-
-		String response = restTemplate.postForObject(url, request, String.class);
-
-		System.out.println(response);
 	}
 
 }
