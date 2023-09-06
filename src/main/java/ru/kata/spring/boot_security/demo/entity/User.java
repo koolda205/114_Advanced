00e151402bf11ad10c7f581dfb882475468473ac
Dmitrii Kulakov <koolda@yandex.ru>
2023-09-06 23:35:13 +0300

    comment back         public String roleToString() {             return roles.stream().map(Object::toString).collect(Collectors.joining(", "));         }         @Override         public Collection<? extends GrantedAuthority> getAuthorities() {             return getRoles();         }

--- src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/User.java
@@ -127,13 +127,13 @@ public class User implements UserDetails {
         }
         return sb.toString();
     }
-//    public String roleToString() {
-//        return roles.stream().map(Object::toString).collect(Collectors.joining(", "));
-//    }
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return getRoles();
-//    }
+    public String roleToString() {
+        return roles.stream().map(Object::toString).collect(Collectors.joining(", "));
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
 
     @Override
     public boolean isAccountNonExpired() {
